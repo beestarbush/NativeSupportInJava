5ecdf524a6402c5f114257f4c26f6a8c61d6c697
randybnl <randybijsterbosch@live.nl>
2015-04-06 15:23:45 +0200

    Small changes

--- NativeClassImp.c
+++ NativeClassImp.c
@@ -3,6 +3,6 @@
 #include <stdio.h>
 
 JNIEXPORT void JNICALL Java_NativeClass_printText(JNIEnv *env, jobject obj) {
-	printf("It works, this function is called from a native method in C!");
+	printf("It works, this function is called from a native method in C!\n");
 	return;
 }
